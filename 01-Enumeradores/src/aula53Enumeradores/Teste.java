653f2315e662ee66eb989cc8f76f11f2da5234c0
José Malcher Jr <malcher.malch@gmail.com>
2016-10-07 11:04:40 -0300

    Aula 53 - ENUM - Parte 1 - Constantes normal, sem enum

---
+++ 01-Enumeradores/src/aula53Enumeradores/Teste.java
@@ -0,0 +1,54 @@
+package aula53Enumeradores;
+
+public class Teste {
+	public static void main(String[] args) {
+		usandoConstantes();
+	}
+
+	private static void usandoConstantes() {
+
+		int segunda = DiaSemanaConstantes.SEGUNDA;
+		int terca = DiaSemanaConstantes.TERCA;
+		int quarta = DiaSemanaConstantes.QUARTA;
+		int quinta = DiaSemanaConstantes.QUINTA;
+		int sexta = DiaSemanaConstantes.SEXTA;
+		int sabado = DiaSemanaConstantes.SABADO;
+		int domingo = DiaSemanaConstantes.DOMINGO;
+
+		System.out.println("Teste utilizando constantes no Java");
+		imprimeDiaSemana(segunda);
+		imprimeDiaSemana(terca);
+		imprimeDiaSemana(quarta);
+		imprimeDiaSemana(quinta);
+		imprimeDiaSemana(sexta);
+		imprimeDiaSemana(sabado);
+		imprimeDiaSemana(domingo);
+	}
+
+	private static void imprimeDiaSemana(int dia) {
+		switch (dia) {
+		case 1:
+			System.out.println("Segunda-feira");
+			break;
+		case 2:
+			System.out.println("Terça-feira");
+			break;
+		case 3:
+			System.out.println("Quarta-feira");
+			break;
+		case 4:
+			System.out.println("Quinta-feira");
+			break;
+		case 5:
+			System.out.println("Sexta-feira");
+			break;
+		case 6:
+			System.out.println("Sábado");
+			break;
+		case 7:
+			System.out.println("Domingo");
+			break;
+		}
+	}
+
+}
