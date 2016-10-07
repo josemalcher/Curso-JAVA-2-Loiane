c343a8d300940a9512f2f088d7b7159a04143030
José Malcher Jr <malcher.malch@gmail.com>
2016-10-07 11:23:10 -0300

    Aula 53 - ENUM - Parte 2 - Usnado classe ENUM

--- 01-Enumeradores/src/aula53Enumeradores/Teste.java
+++ 01-Enumeradores/src/aula53Enumeradores/Teste.java
@@ -3,6 +3,8 @@ package aula53Enumeradores;
 public class Teste {
 	public static void main(String[] args) {
 		usandoConstantes();
+		System.out.println("\n*********\n");
+		usandoEnum();
 	}
 
 	private static void usandoConstantes() {
@@ -50,5 +52,51 @@ public class Teste {
 			break;
 		}
 	}
+
+	private static void usandoEnum() {
+
+		DiaSemana segunda = DiaSemana.SEGUNDA;
+		DiaSemana terca = DiaSemana.TERCA;
+		DiaSemana quarta = DiaSemana.QUARTA;
+		DiaSemana quinta = DiaSemana.QUINTA;
+		DiaSemana sexta = DiaSemana.SEXTA;
+		DiaSemana sabado = DiaSemana.SABADO;
+		DiaSemana domingo = DiaSemana.DOMINGO;
+
+		System.out.println("Teste utilizando enum no Java");
+		imprimeDiaSemana(segunda);
+		imprimeDiaSemana(terca);
+		imprimeDiaSemana(quarta);
+		imprimeDiaSemana(quinta);
+		imprimeDiaSemana(sexta);
+		imprimeDiaSemana(sabado);
+		imprimeDiaSemana(domingo);
+
+	}
+	private static void imprimeDiaSemana(DiaSemana dia){
+		switch (dia) {
+		case SEGUNDA:
+			System.out.println("Segunda-feira");
+			break;
+		case TERCA:
+			System.out.println("Terça-feira");
+			break;
+		case QUARTA:
+			System.out.println("Quarta-feira");
+			break;
+		case QUINTA:
+			System.out.println("Quinta-feira");
+			break;
+		case SEXTA:
+			System.out.println("Sexta-feira");
+			break;
+		case SABADO:
+			System.out.println("Sábado");
+			break;
+		case DOMINGO:
+			System.out.println("Domingo");
+			break;
+		}
+}
 
 }
