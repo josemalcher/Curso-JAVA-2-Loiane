43d1019c053133e4fa3325dd84c1b1476fad45dd
José Malcher Jr <malcher.malch@gmail.com>
2016-10-13 09:38:26 -0300

    Aula 59 - Importação Estática - Static import

--- 02-ELEMENTOS-BASICOS-DA-LINGUAGEM-JAVA/src/aula59importEstatica/StaticImport.java
+++ 02-ELEMENTOS-BASICOS-DA-LINGUAGEM-JAVA/src/aula59importEstatica/StaticImport.java
@@ -13,11 +13,10 @@ public class StaticImport {
 		double c = 4;
 
 		System.out.println(Math.pow(a, b));
-
 		System.out.println(Math.sqrt(c));
 
+		//com os import adicionados
 		System.out.println(pow(a, b));
-
 		System.out.println(sqrt(c));
 
 	}
