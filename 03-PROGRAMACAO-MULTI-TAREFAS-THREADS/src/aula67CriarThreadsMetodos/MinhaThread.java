1dea7550ead7d6b8c06c5932b7cdc95cade07095
José Malcher Jr <malcher.malch@gmail.com>
2016-10-14 11:19:56 -0300

    Aula 67 - Criando Threads + métodos start, run e sleep

---
+++ 03-PROGRAMACAO-MULTI-TAREFAS-THREADS/src/aula67CriarThreadsMetodos/MinhaThread.java
@@ -0,0 +1,29 @@
+package aula67CriarThreadsMetodos;
+
+public class MinhaThread extends Thread {
+
+	private String nome;
+	private int tempo;
+
+	public MinhaThread(String nome, int tempo) {
+		super();
+		this.nome = nome;
+		this.tempo = tempo;
+		start();
+	}
+
+	public void run() {
+		try {
+			for (int i = 0; i < 6; i++) {
+				System.out.println(nome + " contator " + i);
+				Thread.sleep(tempo);
+			}
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+
+		}
+		System.out.println(nome + " terminou a execução ");
+	}
+
+}
