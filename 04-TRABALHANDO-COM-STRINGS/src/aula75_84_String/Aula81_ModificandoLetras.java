9371558db422a0072f27f3399166ab5c205c5ab1
José Malcher Jr <malcher.malch@gmail.com>
2016-10-16 22:00:39 -0300

    Aula 81 - String -  modificando letras maiúsculas/minúsculas (toLowerCase, toUpperCase)

---
+++ 04-TRABALHANDO-COM-STRINGS/src/aula75_84_String/Aula81_ModificandoLetras.java
@@ -0,0 +1,19 @@
+package aula75_84_String;
+
+public class Aula81_ModificandoLetras {
+
+	public static void main(String[] args) {
+		//
+		String teste = "Teste";
+
+		String testeMinusc = teste.toLowerCase();
+		String testeMaisc = teste.toUpperCase();
+
+		System.out.println(testeMinusc);
+		System.out.println(testeMaisc);
+
+		// if (teste.toLowerCase().equals(teste.toLowerCase()))
+
+	}
+
+}
